c0c0e067452abc6fe1a23d86f5e443be34bf5a46
christopher ingram <cingram82@gmail.com>
2021-01-23 14:01:25 -0500

    Mouse look

---
+++ Assets/Scripts/MouseInteract.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInteract : MonoBehaviour
+{
+    public float reachDist = 1f;
+
+    public Camera playerCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, reachDist);
+            Debug.Log(hit.point);
+        }
+    }
+}
